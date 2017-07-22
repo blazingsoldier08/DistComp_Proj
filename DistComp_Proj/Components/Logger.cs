fdf9a3408794974e716425ee4b411e274ee28294
blazingsoldier08 <contagious.thoughts@gmail.com>
2017-07-22 01:35:04 -0400

    Added base C# Project. Completed Logger

---
+++ DistComp_Proj/Components/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistComp_Proj.Components
+{
+    public class Logger
+    {
+        #region SetAttributes
+
+        private static string _processID = null;
+        public static string processID
+        {
+            get { return _processID; }
+            set { _processID = value; }
+        }
+        public static string path
+        {
+            get { return Directory.GetCurrentDirectory() + "\\" + processID + "_msg.txt"; }
+        }
+        private static StreamWriter sw = null;
+        public static StreamWriter StreamWriter
+        {
+            get { return sw; }
+            set { sw = value; }
+        }
+
+        #endregion
+
+        #region
+
+        public static void LogMessage(string bssClock, string message)
+        {
+            using (StreamWriter = File.AppendText(path))
+            {
+                StreamWriter.WriteLine(DateTime.Now + "\n \t Clock Value: " + bssClock + " \n \t Message: " + message);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DistComp_Proj/Program.cs
@@ -0,0 +1,33 @@
+﻿using DistComp_Proj.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistComp_Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TestFileReading
+
+
+
+            #endregion
+
+            #region TestPathing
+
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Logger.processID = args[0];
+            Console.WriteLine(Logger.processID);
+            Console.WriteLine(Logger.path);
+            Logger.LogMessage("1, 2, 3", "test message");
+            Console.ReadLine();
+            
+            #endregion
+        }
+    }
+}
